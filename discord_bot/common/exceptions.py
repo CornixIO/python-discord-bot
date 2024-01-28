96316a72d9ebe67fc635945bdc58108c55b32296
Gilad Wagner <gilad@cornix.io>
2024-01-28 09:08:04 +0200

    add_discord_exceptions

--- discord_bot/common/exceptions.py
+++ discord_bot/common/exceptions.py
@@ -3,3 +3,17 @@ class TooManyRequests(Exception):
         super().__init__(response_json)
         self.response_json = response_json
         self.url = url
+
+
+class DiscordMissingAccess(Exception):
+    pass
+
+
+class DiscordUnknownChannel(Exception):
+    pass
+
+
+CODE_TO_EXCEPTION = {
+    50001: DiscordMissingAccess,
+    10003: DiscordUnknownChannel
+}
